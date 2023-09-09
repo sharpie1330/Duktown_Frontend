d0d7513b4d7676a3b82c53ad3981add063048d89
seoyeon22 <hsy2709@gmail.com>
2023-09-09 21:11:41 +0900

    modify home

--- src/components/Bottombar.js
+++ src/components/Bottombar.js
@@ -2,15 +2,20 @@ import React from 'react';
 import edit from '../assets/edit.png';
 import home from '../assets/home.png';
 import community from '../assets/community.png';
+import edit_blue from '../assets/edit_blue.png';
+import home_blue from '../assets/home_blue.png';
+import community_blue from '../assets/community_blue.png';
 import '../css/Bottombar.css';
 
 function Bottombar(){
     return (
-        <div className='bottombar'>
-            <img src={edit} alt="edit" className="bottombar_icon"/>
-            <img src={home} alt="home" className="bottombar_icon"/>
-            <img src={community} alt="community" className="bottombar_icon"/>
-        </div>
+        <footer>
+            <div className='bottombar'>
+                <img src={edit} alt="edit" className="bottombar_icon"/>
+                <img src={home_blue} alt="home" className="bottombar_icon"/>
+                <img src={community} alt="community" className="bottombar_icon"/>
+            </div>
+        </footer>
     );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import Navbar from '../components/Navbar.js';
 import Bottombar from '../components/Bottombar.js';
+import repair from '../assets/repair.png';
+import register from '../assets/register.png';
+import sleepover from '../assets/sleepover.png';
+import announcement from '../assets/announcement.png';
+import '../css/Home.css';
 
 function Home(){
     return (
         <>
             <Navbar />
             <div className='content-container'>
-                Home
+                <p className='board_title'>공지사항</p>
+                <div className='board'>
+                    내용
+                </div>
+                <p className='board_title'>오늘의 메뉴</p>
+                <div className='board'>
+                    내용
+                </div>
+                <div className='menu-container'>
+                    <div className='menu'>
+                        <img src={repair} className='menu_icon'/>
+                        수리 요청
+                    </div>
+                    <div className='menu'>
+                        <img src={register} className='menu_icon'/>
+                        입사 신청
+                    </div>
+                    <div className='menu'>
+                        <img src={sleepover} className='menu_icon'/>
+                        외박 신청
+                    </div>
+                    <div className='menu'>
+                        <img src={announcement} className='menu_icon'/>
+                        점호 방송
+                    </div>
+                </div>
+                
             </div>
             <Bottombar />
         </> 
